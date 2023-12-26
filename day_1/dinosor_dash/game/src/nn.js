ea466bf580dfaffcf6c8e232d38680fc03b01f3a
shimasan0118 <rio.watanabe@mixi.co.jp>
2023-12-26 03:45:18 +0000

    1日目のワークに必要なものをアップロード

---
+++ day_1/dinosor_dash/game/src/nn.js
@@ -0,0 +1,297 @@
+import 'babel-polyfill';
+import * as tf from '@tensorflow/tfjs';
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from './game/constants';
+import { Runner } from './game';
+
+const START_BUTTON = document.getElementById('start');
+const STATUS = document.getElementById('status');
+const VIDEO = document.getElementById('webcam');
+const ENABLE_CAM_BUTTON = document.getElementById('enableCam');
+const RESET_BUTTON = document.getElementById('reset');
+const TRAIN_BUTTON = document.getElementById('train');
+const MOBILE_NET_INPUT_WIDTH = 224;
+const MOBILE_NET_INPUT_HEIGHT = 224;
+const STOP_DATA_GATHER = -1;
+const CLASS_NAMES = [];
+
+let runner = null;
+// initial setup for the game the  setup function is called when the dom gets loaded
+
+let gatherDataState = STOP_DATA_GATHER;
+let videoPlaying = false;
+const trainingDataInputs = [];
+const trainingDataOutputs = [];
+const examplesCount = [];
+let predict = false;
+let mobilenet;
+
+ENABLE_CAM_BUTTON.addEventListener('click', enableCam);
+TRAIN_BUTTON.addEventListener('click', trainAndPredict);
+RESET_BUTTON.addEventListener('click', reset);
+
+// Call the function immediately to start loading.
+loadMobileNetFeatureModel();
+
+const dataCollectorButtons = document.querySelectorAll('button.dataCollector');
+for (let i = 0; i < dataCollectorButtons.length; i+=1) {
+  dataCollectorButtons[i].addEventListener('mousedown', gatherDataForClass);
+  dataCollectorButtons[i].addEventListener('mouseup', gatherDataForClass);
+  // Populate the human readable names for classes.
+  CLASS_NAMES.push(dataCollectorButtons[i].getAttribute('data-name'));
+}
+
+const model = tf.sequential();
+model.add(tf.layers.dense({inputShape: [1024], units: 128, activation: 'relu'}));
+model.add(tf.layers.dense({units: CLASS_NAMES.length, activation: 'softmax'}));
+
+model.summary();
+
+// Compile the model with the defined optimizer and specify a loss function to use.
+model.compile({
+  // Adam changes the learning rate over time which is useful.
+  optimizer: 'adam',
+  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+  // Else categoricalCrossentropy is used if more than 2 classes.
+  loss: (CLASS_NAMES.length === 2) ? 'binaryCrossentropy': 'categoricalCrossentropy',
+  // As this is a classification problem you can record accuracy in the logs too!
+  metrics: ['accuracy']
+});
+
+/**
+ * Loads the MobileNet model and warms it up so ready for use.
+ * */
+async function loadMobileNetFeatureModel() {
+  const URL =
+    'https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1';
+  console.log("hoge")
+  mobilenet = await tf.loadGraphModel(URL, {fromTFHub: true});
+  STATUS.innerText = '学習モデルの読み込みに成功しました';
+
+  // Warm up the model by passing zeros through it once.
+  tf.tidy(() => {
+    const answer = mobilenet.predict(tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3]));
+    console.log(answer.shape);
+  });
+}
+
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+function enableCam() {
+  if (hasGetUserMedia()) {
+    // getUsermedia parameters.
+    const constraints = {
+      video: true,
+      width: 640,
+      height: 480
+    };
+
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      VIDEO.srcObject = stream;
+      VIDEO.addEventListener('loadeddata', () => {
+        videoPlaying = true;
+        ENABLE_CAM_BUTTON.classList.add('removed');
+      });
+    });
+  } else {
+    console.warn('getUserMedia() is not supported by your browser');
+  }
+}
+
+async function trainAndPredict() {
+  predict = false;
+  tf.util.shuffleCombo(trainingDataInputs, trainingDataOutputs);
+  const outputsAsTensor = tf.tensor1d(trainingDataOutputs, 'int32');
+  const oneHotOutputs = tf.oneHot(outputsAsTensor, CLASS_NAMES.length);
+  const inputsAsTensor = tf.stack(trainingDataInputs);
+
+  const results = await model.fit(inputsAsTensor, oneHotOutputs, {shuffle: true, batchSize: 5, epochs: 10,
+      callbacks: {onEpochEnd: logProgress} });
+
+  outputsAsTensor.dispose();
+  oneHotOutputs.dispose();
+  inputsAsTensor.dispose();
+  predict = true;
+  predictLoop();
+}
+
+function logProgress(epoch, logs) {
+  console.log(`Data for epoch ${epoch}, ${logs}`);
+}
+
+function predictLoop() {
+  if (predict) {
+    tf.tidy(() => {
+      const videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
+      const resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH], true);
+
+      const imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+      const prediction = model.predict(imageFeatures).squeeze();
+      const highestIndex = prediction.argMax().arraySync();
+      const predictionArray = prediction.arraySync();
+
+      STATUS.innerText = `Prediction: ${CLASS_NAMES[highestIndex]} with ${Math.floor(predictionArray[highestIndex] * 100)}% confidence`;
+
+    });
+
+    window.requestAnimationFrame(predictLoop);
+  }
+}
+
+/**
+ * Purge data and start over. Note this does not dispose of the loaded
+ * MobileNet model and MLP head tensors as you will need to reuse
+ * them to train a new model.
+ * */
+function reset() {
+  predict = false;
+  examplesCount.length = 0;
+  for (let i = 0; i < trainingDataInputs.length; i+=1) {
+    trainingDataInputs[i].dispose();
+  }
+  trainingDataInputs.length = 0;
+  trainingDataOutputs.length = 0;
+  STATUS.innerText = 'No data collected';
+
+  console.log(`Tensors in memory: ${tf.memory().numTensors}`);
+}
+
+/**
+ * Handle Data Gather for button mouseup/mousedown.
+ * */
+function gatherDataForClass() {
+  const classNumber = parseInt(this.getAttribute('data-1hot'), 10);
+  gatherDataState = (gatherDataState === STOP_DATA_GATHER) ? classNumber : STOP_DATA_GATHER;
+  dataGatherLoop();
+}
+
+function dataGatherLoop() {
+  if (videoPlaying && gatherDataState !== STOP_DATA_GATHER) {
+    const imageFeatures = tf.tidy(() => {
+      const videoFrameAsTensor = tf.browser.fromPixels(VIDEO);
+      const resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH], true);
+      const normalizedTensorFrame = resizedTensorFrame.div(255);
+      return mobilenet.predict(normalizedTensorFrame.expandDims()).squeeze();
+    });
+
+    trainingDataInputs.push(imageFeatures);
+    trainingDataOutputs.push(gatherDataState);
+
+    // Intialize array index element if currently undefined.
+    if (examplesCount[gatherDataState] === undefined) {
+      examplesCount[gatherDataState] = 0;
+    }
+    examplesCount[gatherDataState]+=1;
+
+    STATUS.innerText = '';
+    for (let n = 0; n < CLASS_NAMES.length; n += 1) {
+      STATUS.innerText += `${CLASS_NAMES[n]} data count: ${examplesCount[n]}. `;
+    }
+    window.requestAnimationFrame(dataGatherLoop);
+  }
+}
+
+function setup() {
+  // Initialize the game Runner.
+  runner = new Runner('.game', {
+    DINO_COUNT: 1,
+    onReset: handleReset,
+    onCrash: handleCrash,
+    onRunning: handleRunning
+  });
+  // Set runner as a global variable if you need runtime debugging.
+  window.runner = runner;
+  // Initialize everything in the game and start the game.
+  runner.init();
+}
+// variable which tells whether thethe game is being loaded for the first time i.e. not a reset
+
+
+function handleReset(dinos) {
+  // running this for single dino at a time
+  // console.log(dinos);
+  
+  const dino = dinos[0]; 
+  // if the game is being started for the first time initiate 
+  // the model and compile it to make it ready for training and predicting
+}
+
+/**
+ * documentation
+ * @param {object} dino
+ * @param {object} state
+ * returns a promise resolved with an action
+ */
+
+function handleRunning( dino, state ) {
+  return new Promise((resolve) => {
+    if (!dino.jumping) {
+      // whenever the dino is not jumping decide whether it needs to jump or not
+      let action = 0;// variable for action 1 for jump 0 for not
+    
+      // predictionArrayを関数の外側で宣言
+      let predictionArray;
+        
+      tf.tidy(() => {
+        const videoFrameAsTensor = tf.browser.fromPixels(VIDEO).div(255);
+        const resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH], true);
+
+        const imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+        const prediction = model.predict(imageFeatures).squeeze();
+        const highestIndex = prediction.argMax().arraySync();
+
+        // tf.tidyブロック内でpredictionArrayを更新
+        predictionArray = prediction.arraySync();
+
+        STATUS.innerText = `Prediction: ${CLASS_NAMES[highestIndex]} with ${Math.floor(predictionArray[highestIndex] * 100)}% confidence`;
+      });
+      
+      if (predictionArray[1] > predictionArray[0]) {
+        // we want to jump
+        action = 1;
+        // set last jumping state to current state
+        dino.lastJumpingState = state;
+      } else {
+        // set running state to current state
+        dino.lastRunningState = state;
+      }
+      resolve(action);
+    } else {
+      resolve(0);
+    }
+  });
+}
+
+/**
+ * 
+ * @param {object} dino 
+ * handles the crash of a dino before restarting the game
+ * 
+ */
+function handleCrash( dino ) {
+  const input = null;
+  const label = null;
+}
+
+/**
+ * 
+ * @param {object} state
+ * returns an array 
+ * converts state to a feature scaled array
+ */
+function convertStateToVector(state) {
+  if (state) {
+    return [
+      state.obstacleX / CANVAS_WIDTH,
+      state.obstacleWidth / CANVAS_WIDTH,
+      state.speed / 100
+    ];
+  }
+  return [0, 0, 0];
+}
+
+// call setup on loading content
+// document.addEventListener('DOMContentLoaded', setup);
+START_BUTTON.addEventListener('click', setup);
---
+++ day_1/speed_sketch_dash/src/components/SketchCanvas.jsx
@@ -0,0 +1,218 @@
+
+
+import { useEffect, useRef, useState, forwardRef, useImperativeHandle } from 'react';
+import { throttle } from '../utils.js';
+
+import constants from '../constants.js';
+
+const START_DRAW_EVENTS = ['mousedown', 'touchstart'];
+const DRAW_EVENTS = ['mousemove', 'touchmove'];
+const STOP_DRAW_EVENTS = ['mouseup', 'mouseout', 'touchend'];
+
+const THROTTLE_MS = 10; // number of milliseconds to throttle drawing by
+
+// Ensure the canvas is at least as large as the screen
+const CANVAS_SIZE = Math.max(window.screen.width, window.screen.height);
+
+const SKETCH_PADDING = 4; // number of pixels around the sketch
+
+const addEventListeners = (item, events, fn) => {
+  for (let event of events) {
+    item.addEventListener(event, fn);
+  }
+}
+
+const removeEventListeners = (item, events, fn) => {
+  for (let event of events) {
+    item.removeEventListener(event, fn);
+  }
+}
+
+const getPosition = (event) => {
+  if (event.touches && event.touches[0]) {
+    // Account for "safe area" on iPhones (i.e., notch)
+    const diff = (event.target.offsetHeight - document.body.offsetHeight) / 2;
+    return [event.touches[0].clientX, event.touches[0].clientY - diff]
+  } else {
+    return [event.offsetX, event.offsetY];
+  }
+}
+
+
+const SketchCanvas = forwardRef(({
+  onSketchChange, disabled
+}, ref) => {
+
+  // const [disabled, setDisabled] = useState(false);
+
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [sketchBoundingBox, setSketchBoundingBox] = useState(null); // [x1, y1, x2, y2]
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const [timeSpentDrawing, setTimeSpentDrawing] = useState(0);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!contextRef.current) {
+      // Set up the canvas context on initial render
+      // NOTE: We set `willReadFrequently` to true to improve performance.
+      contextRef.current = canvas.getContext('2d', { willReadFrequently: true });
+    }
+
+    const context = contextRef.current;
+
+    // Setup the brush
+    context.imageSmoothingEnabled = true;
+    context.lineWidth = constants.BRUSH_SIZE;
+    context.lineJoin = 'round';
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+    context.shadowColor = 'rgba(0, 0, 0, 0.9)'; // Set shadow color
+    context.shadowBlur = 1; // Set shadow blur
+
+    const paddingLeft = (canvas.width - window.innerWidth) / 2;
+    const paddingTop = (canvas.height - window.innerHeight) / 2;
+
+    const brushRadius = constants.BRUSH_SIZE / 2;
+
+    const handleResize = () => {
+      // NOTE: We adjust the style's width and height to avoid clearing the canvas data. 
+      canvas.style.width = window.innerWidth;
+      canvas.style.height = window.innerHeight;
+    };
+
+    const startDrawing = (event) => {
+      if (disabled) return;
+
+      // if (event.button !== 0) return; // Only draw on left click
+      const [offsetX, offsetY] = getPosition(event);
+      const canvasX = offsetX + paddingLeft;
+      const canvasY = offsetY + paddingTop;
+      context.moveTo(canvasX, canvasY);
+      context.beginPath();
+      context.lineTo(canvasX, canvasY);
+
+      // NOTE: Due to a bug with iOS, we just draw a dot on touchstart events
+      context.arc(canvasX, canvasY, 0.5, 0, 2 * Math.PI);
+      context.stroke();
+
+      setIsDrawing(true);
+
+      setSketchBoundingBox(x => x === null
+        ? [canvasX, canvasY, canvasX, canvasY]
+        : [
+          Math.min(x[0], canvasX - brushRadius),
+          Math.min(x[1], canvasY - brushRadius),
+          Math.max(x[2], canvasX + brushRadius),
+          Math.max(x[3], canvasY + brushRadius),
+        ]
+      );
+      onSketchChange();
+    };
+
+    const draw = throttle((event) => {
+      if (!isDrawing || disabled) return;
+
+      setTimeSpentDrawing(x => x + THROTTLE_MS)
+
+      const [offsetX, offsetY] = getPosition(event);
+      const canvasX = offsetX + paddingLeft;
+      const canvasY = offsetY + paddingTop;
+
+      setSketchBoundingBox(x => x === null ? x : [
+        Math.min(x[0], canvasX - brushRadius),
+        Math.min(x[1], canvasY - brushRadius),
+        Math.max(x[2], canvasX + brushRadius),
+        Math.max(x[3], canvasY + brushRadius),
+      ]);
+
+      context.lineTo(canvasX, canvasY);
+      context.stroke();
+      onSketchChange();
+    }, THROTTLE_MS);
+
+    const stopDrawing = () => {
+      setIsDrawing(false);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    addEventListeners(canvas, START_DRAW_EVENTS, startDrawing);
+    addEventListeners(canvas, DRAW_EVENTS, draw);
+    addEventListeners(canvas, STOP_DRAW_EVENTS, stopDrawing);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+
+      removeEventListeners(canvas, START_DRAW_EVENTS, startDrawing);
+      removeEventListeners(canvas, DRAW_EVENTS, draw);
+      removeEventListeners(canvas, STOP_DRAW_EVENTS, stopDrawing);
+    };
+  }, [isDrawing, onSketchChange, disabled]);
+
+
+  const getCanvasData = () => {
+    if (sketchBoundingBox === null) return null;
+
+    const context = contextRef.current;
+
+    // Ensure sketch is square (and that aspect ratio is maintained)
+    let left = sketchBoundingBox[0];
+    let top = sketchBoundingBox[1];
+    let width = sketchBoundingBox[2] - sketchBoundingBox[0];
+    let height = sketchBoundingBox[3] - sketchBoundingBox[1];
+    let sketchSize = 2 * SKETCH_PADDING;
+
+    // Center the crop
+    if (width >= height) {
+      sketchSize += width;
+      top = Math.max(top - (width - height) / 2, 0);
+    } else {
+      sketchSize += height;
+      left = Math.max(left - (height - width) / 2, 0);
+    }
+
+    const imgData = context.getImageData(left - SKETCH_PADDING, top - SKETCH_PADDING, sketchSize, sketchSize);
+
+    // DEBUG:
+    // context.putImageData(imgData, 1920 / 2, 1920 / 2)
+    // context.strokeRect(1920 / 2, 1920 / 2, sketchSize, sketchSize);
+
+    return imgData;
+  };
+
+  const clearCanvas = (resetTimeSpentDrawing = false) => {
+    setSketchBoundingBox(null);
+    const canvas = canvasRef.current;
+    const context = contextRef.current;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    setIsDrawing(false);
+
+    if (resetTimeSpentDrawing) {
+      setTimeSpentDrawing(0);
+    }
+  };
+
+  // Expose the getCanvasData/clearCanvas functions to the parent component
+  useImperativeHandle(ref, () => ({
+    getCanvasData: getCanvasData,
+    clearCanvas: clearCanvas,
+    getTimeSpentDrawing: () => timeSpentDrawing,
+  }));
+
+  return (
+    <canvas
+      className='object-none w-full h-full'
+      ref={canvasRef}
+
+      width={CANVAS_SIZE}
+      height={CANVAS_SIZE}
+    />
+  );
+});
+SketchCanvas.displayName = 'SketchCanvas'; // Add the display name
+
+export default SketchCanvas;
