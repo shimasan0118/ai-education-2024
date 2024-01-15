fbd03fdde03729411d0bcbaf6895e93c96b624ce
nami73b <nami73bnami73@gmail.com>
2024-01-15 16:59:34 +0900

    不要なラベルを削除する対応

--- day_1/work/utils/quick_draw.py
+++ day_1/work/utils/quick_draw.py
@@ -6,7 +6,7 @@ import torch
 import onnxruntime
 from datasets import load_dataset
 from collections import Counter
-from utils.draw_game_labels import get_label_list, get_en_label_list
+from utils.draw_game_labels import LABELS, EN_TO_JA_LABELS
 
 MODEL_IMG_PATH = 'speed_sketch_dash/models/model.png'
 
@@ -14,8 +14,8 @@ MODEL_IMG_PATH = 'speed_sketch_dash/models/model.png'
 class QuickDrawModel:
     def __init__(self):
         self.dataset = load_dataset('Xenova/quickdraw-small')
-        self.labels = get_label_list()
-        self.en_labels = get_en_label_list()
+        self.labels = LABELS
+        self.en_labels = EN_TO_JA_LABELS
         
     def show_dataset_num(self):
         print("学習用データ　　　: {}個".format(self.dataset['train'].num_rows))
@@ -29,7 +29,7 @@ class QuickDrawModel:
         # データセットからラベルを取得
         labels = self.dataset[dataset_type]['label']
         # 各クラスの出現回数をカウント
-        label_counts = Counter(labels)        
+        label_counts = Counter(labels)
         
         # 最少および最多のデータ数を持つクラスを特定
         min_label = min(label_counts, key=label_counts.get)
@@ -52,39 +52,51 @@ class QuickDrawModel:
         plt.show()
 
         # 最少および最多のデータ数を持つクラスの情報を表示
-        print(f"最少データ数のクラス: 【ラベル】 {self.labels[min_label]}, 【総数】 {min_count}")
-        print(f"最大データ数のクラス: 【ラベル】 {self.labels[max_label]}, 【総数】 {max_count}")        
-        
+        print(f"最少データ数のクラス: 【ラベル】 {self.labels[str(min_label)]}, 【総数】 {min_count}")
+        print(f"最大データ数のクラス: 【ラベル】 {self.labels[str(max_label)]}, 【総数】 {max_count}")  
+
     # データセットから画像とラベルを表示するメソッド
     def show_random_images_and_labels(self, n, dataset_type='train', class_label=None):
         plt.figure(figsize=(12, 4))
         selected_indices = []
 
         if class_label is not None:
-            if class_label not in self.labels:
+            if class_label not in self.labels.values():
                 print(f"指定されたクラスラベル '{class_label}' は存在しません。")
                 return
             for i, example in enumerate(self.dataset[dataset_type]):
-                if self.labels[example['label']] == class_label:
-                    found = True
-                    selected_indices.append(i)
-                    if len(selected_indices) == n:  # n枚の画像が見つかったら停止
-                        break       
+                if str(example['label']) in self.labels.keys():
+                    if self.labels[str(example['label'])] == class_label:
+                        found = True
+                        selected_indices.append(i)
+                        if len(selected_indices) == n:  # n枚の画像が見つかったら停止
+                            break
+
+            for i, idx in enumerate(selected_indices):
+                example = self.dataset[dataset_type][idx]
+                image = example['image'].resize((128, 128))
+                label = class_label
+
+                # サブプロットに画像とラベルを表示
+                plt.subplot(1, n, i+1)
+                plt.imshow(image, cmap='gray')
+                plt.title(label)
+                plt.axis('off')  
         else:
             # ランダムにデータを選択
-            selected_indices = random.sample(range(len(self.dataset[dataset_type])), n)
+            selected_label_keys = random.sample(LABELS.keys(), n)
 
-        for i, idx in enumerate(selected_indices):
-            example = self.dataset[dataset_type][idx]
-            image = example['image'].resize((128, 128))
-            label = self.dataset[dataset_type].features['label'].names[example['label']]
-            label = self.labels[self.en_labels.index(label)]
+            for i, label_key in enumerate(selected_label_keys):
+                selected_idx = self.dataset[dataset_type]['label'].index(int(label_key))
+                image = self.dataset[dataset_type][selected_idx]['image']
+                image = image.resize((128, 128))
+                label = self.labels[label_key]
 
-            # サブプロットに画像とラベルを表示
-            plt.subplot(1, n, i+1)
-            plt.imshow(image, cmap='gray')
-            plt.title(label)
-            plt.axis('off')
+                # サブプロットに画像とラベルを表示
+                plt.subplot(1, n, i+1)
+                plt.imshow(image, cmap='gray')
+                plt.title(label)
+                plt.axis('off')
         plt.show()
 
     def plot_model(self):
@@ -99,7 +111,13 @@ class QuickDrawModel:
         
     
     def predict_and_show(self, model, n=10):
-        random_indices = np.random.choice(len(self.dataset['test']), n, replace=False)
+        random_indices = np.random.choice(len(self.dataset['test']), n + 30, replace=False)
+
+        random_indices = [
+            idx
+            for idx in random_indices if str(self.dataset['test'][int(idx)]['label']) in self.labels.keys()
+        ][:n]
+
         fig, axes = plt.subplots(4, 5, figsize=(15, 8))  # 4行5列のグリッド
 
         for i, idx in enumerate(random_indices):
@@ -113,13 +131,26 @@ class QuickDrawModel:
             test_image_norm = np.expand_dims(test_image_norm, 0)
 
             # モデルによる予測
-            results = model.run(["logits"], {"pixel_values": test_image_norm})
-            predicted_label = self.dataset['test'].features['label'].names[np.argmax(results[0])]
-            predicted_label = self.labels[self.en_labels.index(predicted_label)]
+            results = model.run(["logits"], {"pixel_values": test_image_norm})[0][0]
+
+            # ラベルにないものは0で置き換える
+            results = [
+                res if i in [int(l) for l in LABELS.keys()] else 0
+            for i, res in enumerate(results)
+            ]
+
+            predicted_label = self.dataset['test'].features['label'].names[np.argmax(results)]
+            if predicted_label not in self.en_labels.keys():
+                print('pred: ', predicted_label)
+                continue
+            predicted_label = self.en_labels[predicted_label]
 
             # 実際のラベル
             actual_label = self.dataset['test'].features['label'].names[self.dataset['test'][idx]['label']]
-            actual_label = self.labels[self.en_labels.index(actual_label)]
+            if actual_label not in self.en_labels.keys():
+                print('acc: ',actual_label)
+                continue
+            actual_label = self.en_labels[actual_label]
 
             # 画像と予測結果の表示位置を計算
             row = i // 5
